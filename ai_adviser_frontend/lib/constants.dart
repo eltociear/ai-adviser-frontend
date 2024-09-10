49b8a5693189bdab6a23647b19ecf8111bc8c4aa
Ikko Ashimine <ashimine_ikko_bp@tenso.com>
2024-09-10 22:25:08 +0900

    update main.dart
    Signed-off-by: Ikko Ashimine <ashimine_ikko_bp@tenso.com>

--- ai_adviser_frontend/lib/constants.dart
+++ ai_adviser_frontend/lib/constants.dart
@@ -1,3 +1,4 @@
 class AppConstants {
-  static const String apiKey = 'YOUR_API_KEY';
+  static const String apiKey =
+      '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';
 }
